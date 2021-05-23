6cca47706c0e00178b48e1ed6dd0320b47097657
Mauricio Nunes <mauricom7@hotmail.com>
2021-05-23 09:55:55 -0300

    Configurando controllers e rotas de categorias

---
+++ routes/categoria.js
@@ -0,0 +1,12 @@
+const categoria = require('../controllers/categoria')
+const express = require('express');
+
+var router = express.Router();
+
+router.get('/', categoria.index)
+router.get('/:id', categoria.get)
+router.put('/:id', categoria.update)
+router.post('/', categoria.create)
+router.delete('/:id', categoria.delete)
+
+module.exports = router
